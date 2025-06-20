0ccb384c65d770290e6f65cbc68fca845e40f508
Ian Kiprotich <kiprotichian254@gmail.com>
2025-06-20 11:33:00 +0300

    Implement and explain selection sort algorithm

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,15 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+
+    n = len(unsorted_list)
+
+    for i in range(n): 
+        swapped = False
+        for j in range(0, n - i - 1): 
+            if unsorted_list[j] > unsorted_list[j + 1]: # swap if the element found is greater then the next element
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
+                swapped = True
+
+        # if no two elements were swapped by inner loop, then break, because the list is alread sorted
+        if not swapped:
+            break
+    return unsorted_list
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,14 @@
-def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+def selection_sort(unsorted_list):
+
+    n = len(unsorted_list)
+
+    for i in range(n):
+        # assume the minimum is the first element
+        min_index = i
+        for j in range(i + 1, n):
+            if unsorted_list[j] < unsorted_list[min_index]:
+                min_index = j
+        # swap the found minimum element with the first element
+        unsorted_list[i], unsorted_list[min_index] = unsorted_list[min_index], unsorted_list[i]
+        
+    return unsorted_list
